216262fd0e5665d98cc1e3e879206083355185e6
Reiner Tupaz <rstupaz@hotmail.com>
2020-07-22 18:48:09 +0800

    Initial commit

---
+++ Notes/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+    }
+}
